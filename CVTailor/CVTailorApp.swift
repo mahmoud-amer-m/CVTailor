e45ef86894274f5f94d401ae1cb00f1e3a604939
Mahmoud Amer <mahmoud.amer.m@gmail.com>
2026-05-17 19:49:48 +0200

    Initial Commit

---
+++ CVTailor/CVTailorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVTailorApp.swift
+//  CVTailor
+//
+//  Created by Mahmoud Amer on 17.05.26.
+//
+
+import SwiftUI
+
+@main
+struct CVTailorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CVTailor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CVTailor
+//
+//  Created by Mahmoud Amer on 17.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
